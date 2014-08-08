f0151be25d9daab902bbe27e187812c1a80aa77f
lfenster <lfenster@microsoft.com>
2014-08-08 00:58:55 -0400

    made writing to PNG files consistent for all plots
    made writing to PNG files consistent for all plots

--- plot1.R
+++ plot1.R
@@ -2,6 +2,7 @@ source('C:/Repos/ExData_Plotting1/cleanedData.R')
 
 datasource <-cleanedData()
 data<-datasource$get()
+png(filename = 'plot1.png', width = 500, height = 500, units = 'px')
 hist(data$Global_active_power, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
-dev.copy(png, file="plot1.png")   #copy to PNG file
+#dev.copy(png, file="plot1.png")   #copy to PNG file
 dev.off()
--- plot2.R
+++ plot2.R
@@ -3,6 +3,7 @@ source('C:/Repos/ExData_Plotting1/cleanedData.R')
 datasource <-cleanedData()
 data<-datasource$get()
 data$Date <- strptime(paste(data$Date,data$Time), "%Y-%m-%d %H:%M:%S")
+png(filename = 'plot2.png', width = 500, height = 500, units = 'px')
 plot(data$Date, data$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
-dev.copy(png, file="plot2.png")   #copy to PNG file
+#dev.copy(png, file="plot2.png")   #copy to PNG file
 dev.off()
